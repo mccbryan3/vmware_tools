41f96a0da6f772bebf6b81a341a20d941b08b6df
Bryan McClellan <bryan@markiiisys.com>
2019-10-24 14:17:43 -0500

    a space was at the end from the alg above

--- vm.format-uuid_serail_no.js
+++ vm.format-uuid_serail_no.js
@@ -7,7 +7,7 @@ function vm_format_uuid(uuid) {
         if(new_uuid.length == 28) {var del = "-"} else {var del = " "}
         new_uuid += uuid.substring(i, i+2) + del
     }
-    return new_uuid
+    return new_uuid.substring(0, new_uuid.length - 1)
 }
 
 var uuid_bios = vm_format_uuid("421419c6-2f79-5e3b-45b8-11256bf0bd0b")
